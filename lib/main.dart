82f6f86f772801777785a2c76d6239d5c2069a7c
mostafaalfahl <mostafaalfahl45@gmail.com>
2024-12-25 19:39:17 +0200

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        useMaterial3: true,
+      ),
+      home: const Text('Flutter Demo Home Page'),
+    );
+  }
+}
