e88a1d45eda535fdae1749d919d70a32c1cf4924
mostafaalfahl <mostafaalfahl45@gmail.com>
2025-01-29 00:36:03 +0200

    newest books cubit done

--- lib/features/home/presentation/view model/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/view model/newest_books_cubit/newest_books_cubit.dart
@@ -1,9 +1,30 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../data/repos/home_repo.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) => emit(
+        NewestBooksFailure(
+          errorMessage: failure.errorMessage,
+        ),
+      ),
+      (books) => emit(
+        NewestBooksSuccess(
+          books: books,
+        ),
+      ),
+    );
+  }
 }
